91602dfc172a5c1d001426c1b70f8d50c87d37ab
Baseta Tube <134952017+basetatube@users.noreply.github.com>
2023-12-18 21:57:02 +0200

    Update security.py

--- security.py
+++ security.py
@@ -1,11 +1,9 @@
 import os
-from google.colab import runtime
+from google.colab import notebook as colab_notebook
 
 def secure_notebook():
-  """Secures the current Colab notebook by making it read-only."""
-
-  notebook = runtime.get_notebook()
-
-  notebook.set_readonly(True)
+    """Secures the current Colab notebook by making it read-only."""
+    current_notebook = colab_notebook.get_notebook()
+    current_notebook.set_readonly(True)
 
 secure_notebook()
